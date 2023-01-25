1c2df4a946d5e35ae9ed5fd82a6e0c3d2d4efb36
J4F3ET <jslopezz@correo.udistrital.edu.co>
2023-01-25 09:04:26 -0500

    Restructurando

--- modelo/app.js
+++ modelo/app.js
@@ -1,21 +1,23 @@
 import express from 'express';
-import {conn} from './db.js';
 import morgan from 'morgan';
-const app = express();
-app.use(morgan());
-app.get("/",async (req,res) => {
-   const r =  await conn.query("SELECT usuario.nombre AS usuario, materia.nombre as materia FROM usuario JOIN usuario_materia ON(usuario_materia.id_usuario=usuario.id) JOIN materia ON(materia.id = usuario_materia.id_materia) WHERE usuario.id = 1;")
-   res.json(r);
-});
-app.post('/submition', (req, res) => {
-    const { username, password } = req.body;
-    // Do something with the data
-    res.send(`Welcome ${username}`);
-});
-app.post('/solicitando_materias_de_usuario',(req,res)=>{
+import empleoyees from './router/empleoyees_router.js';
+import login from './router/login_router.js';
+import ejs from 'ejs';
+import path from 'path';
 
+// const ejs = require('ejs');
+const app = express();
+// MIDDLEWARES
+// app.set('view engine', 'ejs');
+// app.use('/',(req,res,next)=>{});
+app.set('view engine', 'ejs');
+app.use(morgan());
+// ENROUTERS
+const currentDir = path.dirname(import.meta.url.split(':')[1]);
+var loginPath = path.join(currentDir,'\\vista\\interfaces\\login.ejs');
+app.get('/', function (req, res) {
+    res.render(loginPath);
 });
-app.post('/agregar_horario_post',(req,res)=>{
-    
-});
-app.listen("5500");
+app.use(login);
+app.listen("5501");
+console.log("Escucha");
