20dba3ab4dba8d4880342183a309e45c3661dc0c
J4F3ET <jslopezz@correo.udistrital.edu.co>
2023-01-26 07:14:52 -0500

    Configuracion servidor, reder de estilos y javascrip

--- model/app.js
+++ model/app.js
@@ -1,23 +1,24 @@
-import express from 'express';
-import morgan from 'morgan';
-import empleoyees from './router/empleoyees_router.js';
-import login from './router/login_router.js';
-import ejs from 'ejs';
-import path from 'path';
-
-// const ejs = require('ejs');
+// import express from 'express';
+// import morgan from 'morgan';
+// import login from './router/login_router.js';
+// import path from 'path';
+const express = require('express');
+const morgan = require('morgan');
+const login = require('./router/login_router.js');
+const path = require('path');
 const app = express();
+
 // MIDDLEWARES
-// app.set('view engine', 'ejs');
-// app.use('/',(req,res,next)=>{});
-app.set('view engine', 'ejs');
+console.log(path.join(path.resolve(__dirname, '..', 'controllers')));
 app.use(morgan());
+app.set('view engine', 'ejs');
+app.set('views',path.join(path.resolve(__dirname, '..', 'views'),'public'));
+app.use(express.static('views'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 // ENROUTERS
-const currentDir = path.dirname(import.meta.url.split(':')[1]);
-var loginPath = path.join(currentDir,'\\vista\\interfaces\\login.ejs');
-app.get('/', function (req, res) {
-    res.render(loginPath);
-});
+
 app.use(login);
-app.listen("5501");
-console.log("Escucha");
+
+app.listen("5500");
--- model/controllers_router/login_controllers.js
+++
@@ -1,4 +0,0 @@
-import {conn} from '../data_base/db.js';
-export const get_login_user = (req, res) => {
-    res.send('login,path.join(__dirname,login)');
-};
