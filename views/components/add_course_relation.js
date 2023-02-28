51dff97d2b69ec3a20b7396adac7ad01d2e1666b
J4F3ET <jslopezz@udistrital.edu.co>
2023-02-28 07:52:26 -0500

    Agregando creador de select ADDCourse

--- views/components/add_course_relation.js
+++ views/components/add_course_relation.js
@@ -35,8 +35,12 @@ document.addEventListener("DOMContentLoaded", async () => {
 	const input_materia_beneficio = document.querySelector(
 		"[data-add-course-beneficts]"
 	);
+	const botton_agregar_requerimientos = document.querySelector('#botton_agregar_requerimientos');
 	const input_semestre = document.querySelector("[data-add-course-semester]");
+	const botton_agregar_beneficio = document.querySelector('#botton_agregar_beneficios');
 	bloquear(input_materia_beneficio);
+	bloquear(botton_agregar_beneficio);
+	bloquear(botton_agregar_requerimientos);
 	bloquear(input_materia_requisito);
 	const semestres = [];
 	for (let i in data) {
@@ -71,15 +75,60 @@ document.addEventListener("DOMContentLoaded", async () => {
 			if (input_semestre.value <= 0 ){
 				bloquear(input_materia_requisito);
 				bloquear(input_materia_beneficio);
+				bloquear(botton_agregar_beneficio);
+				bloquear(botton_agregar_requerimientos);
 			} else if (parseInt(input_semestre.value) == parseInt(semestres[0])){
 				habilitar(input_materia_beneficio);
 				bloquear(input_materia_requisito);
+				habilitar(botton_agregar_beneficio);
+				bloquear(botton_agregar_requerimientos);
 			} else if (parseInt(input_semestre.value) >= parseInt(semestres[semestres.length - 1])){
 				habilitar(input_materia_requisito);
 				bloquear(input_materia_beneficio);
+				bloquear(botton_agregar_beneficio);
+				habilitar(botton_agregar_requerimientos);
 			}else{
 				habilitar(input_materia_requisito);
 				habilitar(input_materia_beneficio);
+				habilitar(botton_agregar_beneficio);
+				habilitar(botton_agregar_requerimientos);
 			}
 		});
+		document.querySelector('#botton_agregar_requerimientos').addEventListener('click', () => {
+			crearSelectRequerimiento(data, input_semestre, 0, 0);
+		});
 });
+/*<label for="requerimientos_0">Espacios que necesita</label>
+						<select data-add-course-requirements name="requerimientos_0" class="espacios_especiales input" id="requerimientos_0">
+							<option selected value="">Ninguno</option>
+						</select>
+*/
+function crearSelect(data, input_semestre, i,tipoSelect) {
+	if(tipoSelect == 0){
+		type = "requerimientos_"+i;
+		atributo = "data-add-course-requirements";
+	}else if(tipoSelect == 1){
+		type = "beneficios_" + i;
+		atributo = "data-add-course-beneficts";
+	}
+	let select = document.createElement("select");
+	select.classList.add("espacios_especiales input");
+	select.setAttribute(atributo);
+	select.name = type;
+	select.id = type;
+	for (let i in data) {
+		if ( tipoSelect == 0 && parseInt(input_semestre.value) > parseInt(data[i].semestre)) {
+			let option = document.createElement("option");
+			option.value = data[i].id_materia;
+			option.text = data[i].materia;
+			select.options.add(option);
+		} else if (tipoSelect == 1 && parseInt(input_semestre.value) < parseInt(data[i].semestre)) {
+			let option = document.createElement("option");
+			option.value = data[i].id_materia;
+			option.text = data[i].materia;
+			select.options.add(option);
+		}
+	}	
+	select.required = true;
+	return select;
+}
