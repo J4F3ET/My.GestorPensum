6a0505b4311aeed7aaedf58c4cdec373a713b6a0
J4F3ET <jslopezz@udistrital.edu.co>
2023-04-12 17:02:53 -0500

    Refactorizando cod

--- model/router/add_course_router.js
+++ model/router/add_course_router.js
@@ -12,7 +12,6 @@ router.post("/return_materia_requisito", auntenticando, async (req, res) => {
 		[decode.userId]
 	);
 	if (!materia) return res.json({message: "No posee materias"});
-	console.log(materia);
 	return res.json(materia);
 });
 router.post("/materias_del_usuario", auntenticando, async(req, res) => {
@@ -35,28 +34,44 @@ router.get("/add_course", auntenticando, (req, res) => {
 	res.render("add_course");
 });
 router.post("/add_course_pensum", auntenticando, async(req, res) => {
-	if (!req.cookies.DataLogin) return res.json({message: "Error en cookies"});
+	if (!req.cookies.DataLogin) return res.json({message: "Error en cookies"}).res.redirect('/login');
 	console.log(req.body.color);
 	const decode = jwt.verify(req.cookies.DataLogin, secret);
-	const [materia] = await conn.execute(
-		`INSERT INTO materia
-		(nombre, semestre, tipo, creditos, HTD, HTA, HTC, color, estado, usuario) 
-		VALUES (?,?,?,?,?,?,?,?,?,?)`,
-		[
-			req.body.nombre,
-			req.body.semestre,
-			req.body.tipo,
-			req.body.creditos,
-			req.body.HTD,
-			req.body.HTA,
-			req.body.HTC,
-			req.body.color,
-			1,
-			decode.userId
-		]
-	);
-	if (!materia) return res.json({message: "No posee materias"});
-	res.redirect("/index");
+	// const [materia] = await conn.execute(
+	// 	`INSERT INTO materia
+	// 	(nombre, semestre, tipo, creditos, HTD, HTA, HTC, color, estado, usuario) 
+	// 	VALUES (?,?,?,?,?,?,?,?,?,?)`,
+	// 	[
+	// 		req.body.nombre,
+	// 		req.body.semestre,
+	// 		req.body.tipo,
+	// 		req.body.creditos,
+	// 		req.body.HTD,
+	// 		req.body.HTA,
+	// 		req.body.HTC,
+	// 		req.body.color,
+	// 		1,
+	// 		decode.userId
+	// 	]
+	// );
+	// if (!materia) return res.json({message: "Error en registrar materia"});
+	// if (req.body.requerimientos_0 || req.body.requerimientos_0 != ""){
+	// 	for (let i = 0; i < req.body.requerimientos_0.length; i++){
+	// 		const [materia_relacion] = await conn.execute(
+	// 			`INSERT INTO materia_relacion
+	// 			VALUES (?,?)`,
+	// 			[
+	// 				materia.insertId,
+	// 				req.body.'requerimientos_${i}',
+	// 			]
+	// 		);
+	// 	}
+	// }
+	// if (!req.body.beneficios_0 || req.body.beneficios_0 == "") return res.redirect("/index");	
+	// console.log(materia);
+	console.log(req.body);	
+	res.end();
+	// res.redirect("/index");
 });
 
 module.exports = router;
--- views/components/add_course_relation.js
+++ views/components/add_course_relation.js
@@ -1,3 +1,11 @@
+var data;
+var semestres = [];
+let i_requisito = 1;
+let i_beneficio = 1;
+const input_semestre = document.querySelector("[data-add-course-semester]");
+var selectR = document.querySelector(".select_input_requisito");
+var selectB = document.querySelector(".select_input_beneficio");
+var btnRemoveM_V = document.querySelectorAll(".btn_select_remove");
 function getCookie(name) {
 	var value = "; " + document.cookie;
 	var parts = value.split("; " + name + "=");
@@ -9,7 +17,7 @@ function habilitar(input) {
 	input.disabled = false;
 }
 function habilitarV(input) {
-	for (var i =0 ;i < input.length;i++) {
+	for (var i = 0; i < input.length; i++) {
 		input[i].classList.remove("input_bloqueado");
 		input[i].requiered = true;
 		input[i].disabled = false;
@@ -21,20 +29,70 @@ function bloquear(input) {
 	input.requiered = false;
 }
 function bloquearV(input) {
-	console.log('Lo que le llago a bloquearV')
-	console.log(input);
-	for (var i=0 ; i < input.length;i++){
-		console.log(i)
-		console.log(input[i]);
+	for (var i = 0; i < input.length; i++) {
 		input[i].classList.add("input_bloqueado");
 		input[i].disabled = true;
 		input[i].requiered = false;
 	}
 }
+function clearSelect(select){
+	let optionsToRemove = Array.from(select.options);
+	optionsToRemove.forEach(option => select.remove(option));
+}
+function updateListBtnRemoveM() {
+	btnRemoveM_V = document.querySelectorAll(".btn_select_remove");
+	btnRemoveM_V.forEach((element) => {
+		//cuando se pasa el mouse por encima del boton se cambia el texto
+		element.addEventListener("mouseenter", () => {});
+		//cuando se hace click en el boton se elimina el input y se agrega al select
+		element.addEventListener("click", () => {
+			btnRemoveM_Delete(element);
+		});
+	});
+}
+function btnRemoveM_Delete(div){
+	let id_materia = div.getAttribute("data-id_materia");
+	let name_materia = div.innerHTML;
+	let option = document.createElement("option");
+	let semestrediv = div.getAttribute("data-semestre-div");
+	option.value = id_materia;
+	option.text = name_materia;
+	option.setAttribute("data-semestre-option", semestrediv);
+	let input = document.querySelector(`input[value="${id_materia}"]`);
+	input.remove();
+	div.remove();
+	//True = requisito , False = beneficio
+	input_semestre.value > semestrediv
+		? selectR.options.add(option)
+		: selectB.options.add(option);
+}
+function addMateriaRelacion(relacion, id_materia, name_materia, semestre) {
+	var divHidden = document.querySelector(".contenedor_input_hidden");
+	var input = document.createElement('input')
+	input.hidden = true
+	input.name = "materiaRelacion[]"
+	input.type = 'text'
+	input.setAttribute('value', id_materia)
+	divHidden.appendChild(input)
+	var div = document.createElement("div");
+	div.classList.add("btn_select_remove");
+	div.classList.add("btn");
+	div.setAttribute("data-semestre-div", semestre)
+	div.setAttribute("data-id_materia", id_materia)
+	div.innerHTML = `${name_materia}`;
+	//True = requisito , False = beneficio
+	relacion
+		?document
+			.querySelector(".div_form_requisito")
+			.insertAdjacentElement("beforeend", div)
+		:document
+			.querySelector(".div_form_beneficio ")
+			.insertAdjacentElement("beforeend", div)
+		;
+	updateListBtnRemoveM();
+}
+// Peticion para saber las materias del usuario
 document.addEventListener("DOMContentLoaded", async () => {
-	//Metodo que jerarquiza las materias
-	let i_requisito = 1;
-	let i_beneficio = 1;
 	let currentUrl = new URL(window.location.href);
 	const response = await fetch(
 		currentUrl.origin + "/return_materia_requisito",
@@ -47,119 +105,61 @@ document.addEventListener("DOMContentLoaded", async () => {
 			body: "",
 		}
 	);
-	const data = await response.json();
-	const input_materia_requisito = document.querySelectorAll(
-		"[data-add-course-requirements]"
+	data = await response.json();
+	data.forEach((materia) =>
+		!semestres.includes(materia.semestre)
+			? semestres.push(materia.semestre)
+			: null
 	);
-	const input_materia_beneficio = document.querySelectorAll(
-		"[data-add-course-beneficts]"
-	);
-	const botton_agregar_requerimientos = document.querySelector('#botton_agregar_requerimientos');
-	const input_semestre = document.querySelector("[data-add-course-semester]");
-	const botton_agregar_beneficio = document.querySelector('#botton_agregar_beneficios');
-	bloquearV(input_materia_beneficio);
-	bloquear(botton_agregar_beneficio);
-	bloquear(botton_agregar_requerimientos);
-	bloquearV(input_materia_requisito);
-	const semestres = [];
-	for (let i in data) {
-		if (!semestres.includes(data[i].semestre))
-			semestres.push(data[i].semestre);
-	}
 	semestres.sort((a, b) => {
 		return a - b;
 	});
-	//Metodo que bloquea los inputs de requisito y beneficio y agregar los posibles requisitos y beneficios
-	document
-		.querySelector("[data-add-course-semester]")
-		.addEventListener("change", () => {
-			for(var i ;i< input_materia_beneficio.length;i++){
-				let optionsToDeleteBeneficio = Array.from(
-					input_materia_beneficio[i].options
-				).slice(1);
-				optionsToDeleteBeneficio.forEach((option) => option.remove());
-			}
-			for (var i; i < input_materia_requisito.length; i++) {
-				let optionsToDeleteRequisito = Array.from(
-					input_materia_requisito[i].options
-				).slice(1);
-				optionsToDeleteRequisito.forEach((option) => option.remove());
-			}
-			for (let i in data) {
-				if(parseInt(input_semestre.value) > parseInt(data[i].semestre)){
-					let option = document.createElement("option");
-					option.value = data[i].id_materia;
-					option.text = data[i].materia;
-					for(var y=0;y<input_materia_requisito.length;y++)
-						input_materia_requisito[y].options.add(option)
-				}else if(parseInt(input_semestre.value) < parseInt(data[i].semestre)){
-					let option = document.createElement("option");
-					option.value = data[i].id_materia;
-					option.text = data[i].materia;
-					for (var y = 0; y < input_materia_beneficio.length; y++)
-						input_materia_beneficio[y].options.add(option);
-				}
-			}
-			if (input_semestre.value <= 0 ){
-				bloquearV(input_materia_requisito);
-				bloquearV(input_materia_beneficio);
-				bloquear(botton_agregar_beneficio);
-				bloquear(botton_agregar_requerimientos);
-			} else if (parseInt(input_semestre.value) == parseInt(semestres[0])){
-				habilitarV(input_materia_beneficio);
-				bloquearV(input_materia_requisito);
-				habilitar(botton_agregar_beneficio);
-				bloquear(botton_agregar_requerimientos);
-			} else if (parseInt(input_semestre.value) >= parseInt(semestres[semestres.length - 1])){
-				habilitarV(input_materia_requisito);
-				bloquearV(input_materia_beneficio);
-				bloquear(botton_agregar_beneficio);
-				habilitar(botton_agregar_requerimientos);
-			}else{
-				habilitarV(input_materia_requisito);
-				habilitarV(input_materia_beneficio);
-				habilitar(botton_agregar_beneficio);
-				habilitar(botton_agregar_requerimientos);
-			}
-		});
-		document.querySelector('#botton_agregar_requerimientos').addEventListener('click', () => {
-			const select = crearSelect(data, input_semestre, i_requisito, 0);
-			i_requisito++;
-			document.querySelector('#botton_agregar_requerimientos').insertAdjacentElement('beforebegin', select);
-		});
-		document.querySelector("#botton_agregar_beneficios").addEventListener('click', () => {
-			const select = crearSelect(data, input_semestre, i_beneficio, 1);
-			i_beneficio++;
-			document.querySelector('#botton_agregar_beneficios').insertAdjacentElement('beforebegin', select);
-		});
 });
-function crearSelect(data, input_semestre, i,tipoSelect) {
-	if(tipoSelect == 0){
-		type = "requerimientos_"+i;
-		atributo = "data-add-course-requirements";
-	}else if(tipoSelect == 1){
-		type = "beneficios_" + i;
-		atributo = "data-add-course-beneficts";
-	}
-	let select = document.createElement("select");
-	select.classList.add("espacios_especiales");
-	select.classList.add("input");
-	select.setAttribute(atributo,"");
-	select.name = type;
-	select.id = type;
+//Metodo que bloquea los inputs de requisito y beneficio y agregar los posibles requisitos y beneficios
+input_semestre.addEventListener("change", () => {
+	//Reset permiso
+	bloquear(selectR);
+	bloquear(selectB);
+	//Reset de los selects
+	clearSelect(selectB)
+	clearSelect(selectR)
+
+	//Seccion que reparte las materias en los selects
 	for (let i in data) {
-		if ( tipoSelect == 0 && parseInt(input_semestre.value) > parseInt(data[i].semestre)) {
-			let option = document.createElement("option");
-			option.value = data[i].id_materia;
-			option.text = data[i].materia;
-			select.options.add(option);
-		} else if (tipoSelect == 1 && parseInt(input_semestre.value) < parseInt(data[i].semestre)) {
-			let option = document.createElement("option");
-			option.value = data[i].id_materia;
-			option.text = data[i].materia;
-			select.options.add(option);
-		}
-	}	
-	select.required = true;
-	return select;
-}
+		let option = document.createElement("option");
+		option.value = data[i].id_materia;
+		option.text = data[i].materia;
+		option.setAttribute("data-semestre-option", data[i].semestre);
+		input_semestre.value > data[i].semestre
+			? selectR.options.add(option)
+			: selectB.options.add(option);
+	}
+
+	//Dar permisos
+	if (input_semestre.value == semestres[0]){
+		habilitar(selectB);
+	}else if(input_semestre.value >= semestres[-1]){
+		habilitar(selectR);
+	}else{
+		habilitar(selectR);
+		habilitar(selectB);
+	}
+});
+//Metodo que ermite agregar la materia seleccionada a la lista de materias
+selectR.addEventListener("change", () => {
+		addMateriaRelacion(
+			true,
+			selectR.options[selectR.selectedIndex].value,
+			selectR.options[selectR.selectedIndex].text,
+			selectR.options[selectR.selectedIndex].getAttribute("data-semestre-option")
+		);
+		selectR.remove(selectR.options[selectR.selectedIndex]);
+	});
+selectB.addEventListener("change", () => {
+		addMateriaRelacion(
+			false,
+			selectB.options[selectB.selectedIndex].value,
+			selectB.options[selectB.selectedIndex].text
+		);
+		selectB.remove(selectB.options[selectB.selectedIndex]);
+});
