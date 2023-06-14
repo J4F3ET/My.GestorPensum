903a18b179597d202e280e8bca121678498fbaee
Laura Aponte <aldanielago@gmail.com>
2023-06-13 21:24:16 -0500

    [Refactorizacion] Total ;v

---
+++ views/lib/js/nodes.js
@@ -0,0 +1,10 @@
+const $ = (id) => document.querySelector(id);
+
+// No olvides poner los puntitos para evitar dolores de cabeza por fa
+// nav completo
+const section_nav_completo = $('.nav_completo');
+const a_nav_icono = $('.nav__icono');
+const a_nav_materias = $('.nav__materias');
+const a_nav__horario = $('nav__horario');
+const a_nav_estadisticas = $('nav_estadisticas');
+const a_nav_cerrar = $('nav_cerrar');
