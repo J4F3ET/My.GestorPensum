746bed79653455514b39107b8b1773abbd0019c4
Laura Aponte <aldanielago@gmail.com>
2023-06-14 22:25:02 -0500

    [Funcionalidad] Navegacion en mobile nav

---
+++ views/lib/js/navigation.js
@@ -0,0 +1,14 @@
+a_nav_icono.forEach(() => {
+    addEventListener('click', () => {
+
+    });
+});
+
+a_nav_mobile_menu.addEventListener('click', () => {
+    section_nav_completo.classList.remove('invisible');
+    section_nav_mobile.classList.add('invisible');
+});
+
+// header_general_section.classList.remove('header-container--long')
+//     header_general_section.style.background = '';
+//     header_arrow.classList.add('inactive');
--- views/lib/js/nodes.js
+++ views/lib/js/nodes.js
@@ -1,10 +1,19 @@
+// No olvides poner los puntitos para evitar dolores de cabeza por fa
 const $ = (id) => document.querySelector(id);
 
-// No olvides poner los puntitos para evitar dolores de cabeza por fa
 // nav completo
 const section_nav_completo = $('.nav_completo');
-const a_nav_icono = $('.nav__icono');
-const a_nav_materias = $('.nav__materias');
-const a_nav__horario = $('nav__horario');
-const a_nav_estadisticas = $('nav_estadisticas');
-const a_nav_cerrar = $('nav_cerrar');
+
+// se crea un array porque se realiza la misma accion en los 3 casos
+const a_nav_icono = document.querySelectorAll('.nav_icono');
+const a_nav_materias = document.querySelectorAll('.nav__materias');
+const a_nav_horario = document.querySelectorAll('nav__horario');
+const a_nav_estadisticas = document.querySelectorAll('nav_estadisticas');
+const a_nav_cerrar = document.querySelectorAll('nav_cerrar');
+
+// nav desktop
+const section_nav_desktop = $('.nav_desktop');
+
+// nav mobile
+const section_nav_mobile = $('.nav_mobile');
+const a_nav_mobile_menu = $('.nav_mobile__menu');
